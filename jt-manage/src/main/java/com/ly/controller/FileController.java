31c7ad171fd2a754148fbec1706dacbdfeb7da1b
lmy1965673628 <1965673628@qq.com>
2020-04-08 22:51:38 +0800

    商品添加完善

--- jt-manage/src/main/java/com/ly/controller/FileController.java
+++ jt-manage/src/main/java/com/ly/controller/FileController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 
 
 /**
@@ -21,6 +23,32 @@ public class FileController {
 	
 	@Autowired
 	private FileService fileService;
+	/**
+	 * SpringMVC负责流操作API
+	 * 1.准备文件路径
+	 * 2.准备文件名称后后缀 abc.jpg
+	 * 3.利用IO流进行写盘操作.
+	 * @param fileImage
+	 * @return
+	 * @throws IOException
+	 * @throws IllegalStateException
+	 */
+	@RequestMapping("/file")
+	public String file(MultipartFile fileImage) throws IllegalStateException, IOException {
+		File dirFile = new File("D:/jt-image");
+		if(!dirFile.exists()) {
+			//如果文件不存在需要新建文件夹/目录
+			//dirFile.mkdir(); //只创建一级目录
+			dirFile.mkdirs();//创建多级目录
+		}
+		//动态获取文件名称
+		String fileName = fileImage.getOriginalFilename();
+		File file = new File("D:/jt-image/"+fileName);
+		//文件上传
+		fileImage.transferTo(file);
+		//转发 重定向不经过视图解析器
+		return "redirect:/easy-ui/easyui-14-upload.html";
+	}
 
 	@RequestMapping("/pic/upload")
 	@ResponseBody
