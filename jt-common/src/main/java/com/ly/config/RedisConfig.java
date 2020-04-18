40e5860145b34e2b62a3705c3d434d59fdea4a3c
lmy1965673628 <1965673628@qq.com>
2020-04-18 08:15:07 +0800

    aop

--- jt-common/src/main/java/com/ly/config/RedisConfig.java
+++ jt-common/src/main/java/com/ly/config/RedisConfig.java
@@ -10,14 +10,18 @@ import redis.clients.jedis.Jedis;
 @Configuration
 @PropertySource("classpath:/properties/redis.properties")
 public class RedisConfig {
-	
-	@Value("${redis.host}")
-	private String host;
-	@Value("${redis.port}")
-	private Integer port;
-	@Bean
-	public Jedis jedis() {
-		
-		return new Jedis(host, port);
-	}
+
+    @Value("${redis.host}")
+    private String host;
+    @Value("${redis.port}")
+    private Integer port;
+    @Value("${redis.password}")
+    private String password;
+
+    @Bean
+    public Jedis jedis() {
+        Jedis jedis = new Jedis(host, port);
+        jedis.auth(password);
+        return jedis;
+    }
 }
