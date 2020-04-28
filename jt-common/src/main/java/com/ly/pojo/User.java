c9642fd38fc9024f554e6c616c4233add3b77b24
lmy1965673628 <1965673628@qq.com>
2020-04-28 23:52:45 +0800

    user pojo对象创建

---
+++ jt-common/src/main/java/com/ly/pojo/User.java
@@ -0,0 +1,35 @@
+package com.ly.pojo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@TableName("tb_user")
+@Data
+@Accessors(chain = true)
+public class User extends BasePojo {
+    /**
+     * 用户Id号  主键自增
+     */
+    @TableId(type = IdType.AUTO)
+    private Long id;
+    /**
+     * 用户名
+     */
+    private String username;
+    /**
+     * 密码
+     */
+    private String password;
+    /**
+     * 电话
+     */
+    private String phone;
+    /**
+     * 邮箱
+     */
+    private String email;
+
+}
