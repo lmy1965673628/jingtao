1f52bc8999bee4830c866c7252c7b25ff6cc833d
lmy1965673628 <1965673628@qq.com>
2020-04-28 23:47:43 +0800

    单点登录系统搭建

---
+++ jt-sso/src/test/java/com/ly/JtSsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JtSsoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
