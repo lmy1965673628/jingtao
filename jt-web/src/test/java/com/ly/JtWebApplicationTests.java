2da7cb341a4c6839f1898bfd494b49fc8b164f45
lmy1965673628 <1965673628@qq.com>
2020-04-28 22:13:10 +0800

    httpclient测试

--- jt-web/src/test/java/com/ly/JtWebApplicationTests.java
+++ jt-web/src/test/java/com/ly/JtWebApplicationTests.java
@@ -1,13 +1,31 @@
 package com.ly;
 
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
 
 @SpringBootTest
 class JtWebApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void doGet() throws ClientProtocolException, IOException {
+        String url = "https://www.baidu.com/";
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpGet get = new HttpGet(url);
+        CloseableHttpResponse response = client.execute(get);
+        if (200 == response.getStatusLine().getStatusCode()) {
+            System.out.println("请求调用成功!!!");
+            String result = EntityUtils.toString(response.getEntity());
+            System.out.println(result);
+        }
     }
+
 
 }
